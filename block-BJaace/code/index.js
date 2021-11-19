5e01d4c92a51bc9d4c89fb1a11d2b61ae2298e9f
atul1104 <waghamareatul2@gmail.com>
2021-11-20 01:59:57 +0530

    Assignment-Operation on arrays

--- block-BJaace/code/index.js
+++ block-BJaace/code/index.js
@@ -10,11 +10,11 @@ console.log (colors[0]);
 // 5. Access the third color in the array and print it to the console using `console.log()`
 console.log (colors[2]);
 // 6. Access the last color in the array and print it to the console using `console.log()`
-console.log (colors[4]);
+console.log (colors.length - 1);
 // 7. Write one line of code that changes the value of the first color in the list to "black" (overwriting the previous value).
 colors[0] = "black";
 // 8. Write one line of code that changes the value of the last color in the list to "ultraviolet" (overwriting the previous value).
-colors[4] = "ultraviolet";
+colors[colors.length - 1] = "ultraviolet";
 // 9. Now, Find the length of the array named `colors`.
 console.log(colors.length);
 // 10. Create a new variable called fourthColor and set it equal to the fourth color in the list.
@@ -36,14 +36,14 @@ for(let i = 0; i < colors.length; i++){
 }
 // 17. Copying from that loop you just wrote, modify it to print every color's value and every color's index in this format: 3, purple or 0, blue etc.
 for(let i = 0; i < colors.length; i++){
-    console.log (colors[i]);
+    console.log (`${i} : ${colors[i]}`);
 }
 // 18. Write a for..of loop to iterate through every element in the array and print each element to the console.
 for(color of colors){
     console.log(color);
 }
 // 19. Create a variable named lastColor that will always point to the last element of the colors array, no matter how many colors are in the list. (Hint: make use of the array's length property for this!)
-const lastColor = colors.length; 
+let lastColor = [colors.length - 1]; 
 // 20. Add a new color `tomato` to the index 45. And check the length of the array. Use `console.log` to print.
 colors[45] = "tomato";
 console.log(colors.length);
